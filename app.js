09ef507dc03a202646a7632840511850745ee21a
Muhanned7 <7muhanned1992@gmail.com>
2025-11-03 18:53:19 +0530

    Second commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { readFile } = require("fs");
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get("/", (req,res)=>{
+    res.render('index', {images: 'images/Enhanced_pic.jpg'});
+    /*
+    readFile('./index.ejs',(content, err)=>{
+        if (err){
+            console.log("file could not be read!!")
+        }
+        else {
+            res.send(content);
+        }
+    });
+    */
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`the server is listening on port ${300}`);
+})
+
